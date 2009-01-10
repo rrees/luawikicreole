4d6418120ad5a7b0681840330ffe01ad993b3e68
Robert Rees <spam@shudderfix.com>
2009-01-10 10:30:05 +0000

    Refactored out bold conversion to function

--- testwikicreole.lua
+++ testwikicreole.lua
@@ -8,5 +8,10 @@ TestWikiCreole = {} --class
 		result = WikiCreole:toHtml(creole)
 		assertEquals(result, '<b>bold</b>')
 	end
+	
+	function TestWikiCreole:testShouldConvertDoubleForwardSlashesToItalics()
+		creole = "//italics//"
+		assertEquals(WikiCreole:toHtml(creole), '<i>italics</i>')
+	end
 
 LuaUnit:run()
--- wikicreole.lua
+++ wikicreole.lua
@@ -3,8 +3,21 @@ WikiCreole ={}
 
 	function WikiCreole:toHtml(creole)
  		-- Converts creole to HTML
-		return string.gsub(creole, "**(%a+)**", function(content)
+		result = WikiCreole:convertItalics(creole)
+		result =  WikiCreole.convertBold(result)
+		return result
+	end
+	
+	function WikiCreole:convertBold(creole)
+		return string.gsub(result, "%*%*(%a+)%*%*", function(content)
 			return string.format("<b>%s</b>", content)
+			end
+		)
+	end
+	
+	function WikiCreole:convertItalics(creole)
+		return string.gsub(creole, "//(%a+)//", function(content)
+				return string.format("<i>%s</i>", content)
 			end
 		)
 	end
